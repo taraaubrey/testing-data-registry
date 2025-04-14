8ac838cfb447e791a603eb8586717207ee7efab1
taraaubrey <tara.a.forstner@gmail.com>
2025-04-15 10:53:44 +1200

    resetting to normal data

--- run_update.bat
+++ run_update.bat
@@ -1,5 +1,5 @@
-dvc diff --show-hash > dvc_diff_status.txt
+dvc diff --show-hash >> dvc_diff_status.txt
 dvc add input_files
 git add .
-git commit -m 'automated: updates to input_files'
+git commit -m 'automated: updates to input_files (added to file)'
 git push origin master
